5226e573d75822c1d104636b3e97144f30959785
Baptiste Amédée Erns <54752685+Uzumakos@users.noreply.github.com>
2022-08-07 11:28:57 -0400

    Update FeaturedPosts.jsx

--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -36,7 +36,7 @@ const FeaturedPosts = () => {
   }, []);
 
   const customLeftArrow = (
-    <div className="absolute arrow-btn left-0 text-center py-2 cursor-pointer rounded-full">
+    <div className="absolute arrow-btn left-2 text-center py-2 cursor-pointer rounded-full">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
       </svg>
@@ -44,7 +44,7 @@ const FeaturedPosts = () => {
   );
 
   const customRightArrow = (
-    <div className="absolute arrow-btn right-0 text-center py-2 cursor-pointer rounded-full">
+    <div className="absolute arrow-btn right-2 text-center py-2 cursor-pointer rounded-full">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
       </svg>
@@ -53,7 +53,7 @@ const FeaturedPosts = () => {
 
   return (
     <div className="mb-8">
-      <Carousel infinite customLeftArrow={customLeftArrow} customRightArrow={customRightArrow} responsive={responsive} itemClass="px-4">
+      <Carousel infinite customLeftArrow={customLeftArrow} customRightArrow={customRightArrow} responsive={responsive} itemClass="px-2">
         {dataLoaded && featuredPosts.map((post, index) => (
           <FeaturedPostCard key={index} post={post} />
         ))}
