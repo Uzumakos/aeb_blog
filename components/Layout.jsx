00af2ca459fde1294d16a834f6660cf9b1d43b27
Baptiste Amédée Erns <54752685+Uzumakos@users.noreply.github.com>
2023-09-01 21:57:37 -0400

    ajoute Layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import { Analytics } from '@vercel/analytics/react';
 
 const Layout = ({ children }) => (
   <>
     <Header />
     {children}
+    <Analytics />
   </>
 );
 
